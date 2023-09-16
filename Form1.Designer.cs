51ed988558b95707f3c5cae6f8f8f54535742ef4
SindreMA <SindreMA@gmail.com>
2023-09-17 00:59:40 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SinTID
+{
+    partial class SinTid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_LoggInn = new System.Windows.Forms.Button();
+            this.button_LoggUt = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label_visuke = new System.Windows.Forms.Label();
+            this.button_NesteUke = new System.Windows.Forms.Button();
+            this.button_Forje = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label__Innidag = new System.Windows.Forms.Label();
+            this.label_utidag = new System.Windows.Forms.Label();
+            this.label_utidag_value = new System.Windows.Forms.Label();
+            this.label_innidag_value = new System.Windows.Forms.Label();
+            this.label_påloggetTid = new System.Windows.Forms.Label();
+            this.label_OverTid = new System.Windows.Forms.Label();
+            this.label_PåloggetTid_value = new System.Windows.Forms.Label();
+            this.label_OverTId_value = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_LoggInn
+            // 
+            this.button_LoggInn.Location = new System.Drawing.Point(12, 12);
+            this.button_LoggInn.Name = "button_LoggInn";
+            this.button_LoggInn.Size = new System.Drawing.Size(286, 37);
+            this.button_LoggInn.TabIndex = 0;
+            this.button_LoggInn.Text = "Logg Inn";
+            this.button_LoggInn.UseVisualStyleBackColor = true;
+            this.button_LoggInn.Click += new System.EventHandler(this.button_LoggInn_Click);
+            // 
+            // button_LoggUt
+            // 
+            this.button_LoggUt.Location = new System.Drawing.Point(344, 12);
+            this.button_LoggUt.Name = "button_LoggUt";
+            this.button_LoggUt.Size = new System.Drawing.Size(312, 37);
+            this.button_LoggUt.TabIndex = 1;
+            this.button_LoggUt.Text = "Logg Ut";
+            this.button_LoggUt.UseVisualStyleBackColor = true;
+            this.button_LoggUt.Click += new System.EventHandler(this.button_LoggUt_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(545, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(45, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label_visuke
+            // 
+            this.label_visuke.AutoSize = true;
+            this.label_visuke.Location = new System.Drawing.Point(497, 61);
+            this.label_visuke.Name = "label_visuke";
+            this.label_visuke.Size = new System.Drawing.Size(42, 13);
+            this.label_visuke.TabIndex = 3;
+            this.label_visuke.Text = "Vis uke";
+            // 
+            // button_NesteUke
+            // 
+            this.button_NesteUke.Location = new System.Drawing.Point(629, 55);
+            this.button_NesteUke.Name = "button_NesteUke";
+            this.button_NesteUke.Size = new System.Drawing.Size(27, 25);
+            this.button_NesteUke.TabIndex = 4;
+            this.button_NesteUke.Text = ">";
+            this.button_NesteUke.UseVisualStyleBackColor = true;
+            this.button_NesteUke.Click += new System.EventHandler(this.button_NesteUke_Click);
+            // 
+            // button_Forje
+            // 
+            this.button_Forje.Location = new System.Drawing.Point(596, 55);
+            this.button_Forje.Name = "button_Forje";
+            this.button_Forje.Size = new System.Drawing.Size(27, 25);
+            this.button_Forje.TabIndex = 5;
+            this.button_Forje.Text = "<";
+            this.button_Forje.UseVisualStyleBackColor = true;
+            this.button_Forje.Click += new System.EventHandler(this.button_Forje_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(671, 248);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label__Innidag
+            // 
+            this.label__Innidag.AutoSize = true;
+            this.label__Innidag.Location = new System.Drawing.Point(12, 52);
+            this.label__Innidag.Name = "label__Innidag";
+            this.label__Innidag.Size = new System.Drawing.Size(89, 13);
+            this.label__Innidag.TabIndex = 7;
+            this.label__Innidag.Text = "Logget inn i dag: ";
+            // 
+            // label_utidag
+            // 
+            this.label_utidag.AutoSize = true;
+            this.label_utidag.Location = new System.Drawing.Point(12, 66);
+            this.label_utidag.Name = "label_utidag";
+            this.label_utidag.Size = new System.Drawing.Size(84, 13);
+            this.label_utidag.TabIndex = 8;
+            this.label_utidag.Text = "Logget ut i dag: ";
+            // 
+            // label_utidag_value
+            // 
+            this.label_utidag_value.AutoSize = true;
+            this.label_utidag_value.Location = new System.Drawing.Point(102, 67);
+            this.label_utidag_value.Name = "label_utidag_value";
+            this.label_utidag_value.Size = new System.Drawing.Size(0, 13);
+            this.label_utidag_value.TabIndex = 9;
+            // 
+            // label_innidag_value
+            // 
+            this.label_innidag_value.AutoSize = true;
+            this.label_innidag_value.Location = new System.Drawing.Point(102, 54);
+            this.label_innidag_value.Name = "label_innidag_value";
+            this.label_innidag_value.Size = new System.Drawing.Size(0, 13);
+            this.label_innidag_value.TabIndex = 10;
+            // 
+            // label_påloggetTid
+            // 
+            this.label_påloggetTid.AutoSize = true;
+            this.label_påloggetTid.Location = new System.Drawing.Point(229, 52);
+            this.label_påloggetTid.Name = "label_påloggetTid";
+            this.label_påloggetTid.Size = new System.Drawing.Size(69, 13);
+            this.label_påloggetTid.TabIndex = 11;
+            this.label_påloggetTid.Text = "Pålogget tid: ";
+            // 
+            // label_OverTid
+            // 
+            this.label_OverTid.AutoSize = true;
+            this.label_OverTid.Location = new System.Drawing.Point(251, 70);
+            this.label_OverTid.Name = "label_OverTid";
+            this.label_OverTid.Size = new System.Drawing.Size(47, 13);
+            this.label_OverTid.TabIndex = 12;
+            this.label_OverTid.Text = "Overtid: ";
+            // 
+            // label_PåloggetTid_value
+            // 
+            this.label_PåloggetTid_value.AutoSize = true;
+            this.label_PåloggetTid_value.Location = new System.Drawing.Point(294, 52);
+            this.label_PåloggetTid_value.Name = "label_PåloggetTid_value";
+            this.label_PåloggetTid_value.Size = new System.Drawing.Size(0, 13);
+            this.label_PåloggetTid_value.TabIndex = 13;
+            // 
+            // label_OverTId_value
+            // 
+            this.label_OverTId_value.AutoSize = true;
+            this.label_OverTId_value.Location = new System.Drawing.Point(294, 70);
+            this.label_OverTId_value.Name = "label_OverTId_value";
+            this.label_OverTId_value.Size = new System.Drawing.Size(0, 13);
+            this.label_OverTId_value.TabIndex = 14;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SinTid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 335);
+            this.Controls.Add(this.label_OverTId_value);
+            this.Controls.Add(this.label_PåloggetTid_value);
+            this.Controls.Add(this.label_OverTid);
+            this.Controls.Add(this.label_påloggetTid);
+            this.Controls.Add(this.label_innidag_value);
+            this.Controls.Add(this.label_utidag_value);
+            this.Controls.Add(this.label_utidag);
+            this.Controls.Add(this.label__Innidag);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_Forje);
+            this.Controls.Add(this.button_NesteUke);
+            this.Controls.Add(this.label_visuke);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_LoggUt);
+            this.Controls.Add(this.button_LoggInn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SinTid";
+            this.Text = "SinTid";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_LoggInn;
+        private System.Windows.Forms.Button button_LoggUt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label_visuke;
+        private System.Windows.Forms.Button button_NesteUke;
+        private System.Windows.Forms.Button button_Forje;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label__Innidag;
+        private System.Windows.Forms.Label label_utidag;
+        private System.Windows.Forms.Label label_utidag_value;
+        private System.Windows.Forms.Label label_innidag_value;
+        private System.Windows.Forms.Label label_påloggetTid;
+        private System.Windows.Forms.Label label_OverTid;
+        private System.Windows.Forms.Label label_PåloggetTid_value;
+        private System.Windows.Forms.Label label_OverTId_value;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+
+namespace SinTID
+{
+    public partial class SinTid : Form
+    {
+        public SinTid()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Loa1d();
+        }
+        public void Loa1d()
+        {
+            lables();
+            string connectionString;
+            connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=#########;password=################";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                MySqlDataAdapter MyDA = new MySqlDataAdapter();
+                string sqlWeek = "SELECT Week from palogging";
+                var cmd = new MySqlCommand(sqlWeek, connection);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (!comboBox1.Items.Contains(reader.GetString("week")))
+                    {
+                        comboBox1.Items.Add(reader.GetString("week"));
+                    }
+                    else { }
+                }
+            }
+            catch { MessageBox.Show("Error"); }
+
+            var culture = CultureInfo.GetCultureInfo("cs-CZ");
+            var dateTimeInfo = DateTimeFormatInfo.GetInstance(culture);
+
+            var dateTime = DateTime.Today;
+
+            int weekNumber = culture.Calendar.GetWeekOfYear(dateTime, dateTimeInfo.CalendarWeekRule, dateTimeInfo.FirstDayOfWeek);
+
+            comboBox1.Text = weekNumber.ToString();
+            try { inn(); } catch { }
+            try { ut(); } catch { }
+        }
+        private void button_LoggInn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString;
+                connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=##########;password=################";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                MySqlCommand cmd = new MySqlCommand();
+                MySqlCommand mcd;
+                string s;
+                string time1 = DateTime.Now.ToShortTimeString().Replace(".", ",");
+                Decimal timedec = Decimal.Parse(time1);
+                Decimal femmin = Decimal.Parse("00,03");
+                Decimal sumdec = timedec - femmin;
+                string time = sumdec.ToString().Replace(",", ".");
+                string date = DateTime.Now.ToShortDateString();
+                string day = DateTime.Now.DayOfWeek.ToString();
+
+                var culture = CultureInfo.GetCultureInfo("cs-CZ");
+                var dateTimeInfo = DateTimeFormatInfo.GetInstance(culture);
+
+                var dateTime = DateTime.Today;
+
+                int weekNumber = culture.Calendar.GetWeekOfYear(dateTime, dateTimeInfo.CalendarWeekRule, dateTimeInfo.FirstDayOfWeek);
+
+                string inorout = "In";
+                s = "INSERT INTO `palogging` (`Time`, `Date`, `Day`, `Week`, `Status`) VALUES('" + time + "', '" + date + "', '" + day + "', '" + weekNumber + "', '" + inorout + "');";
+                connection.Open();
+                cmd = new MySqlCommand(s, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                connection.CloseAsync();
+
+                list();
+                inn();
+            }
+            catch { MessageBox.Show("Error"); }
+        }
+
+        private void button_LoggUt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString;
+                connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=########;password=##########";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                MySqlCommand cmd = new MySqlCommand();
+                MySqlCommand mcd;
+                string s;
+                string time = DateTime.Now.ToShortTimeString();
+                string date = DateTime.Now.ToShortDateString();
+                string day = DateTime.Now.DayOfWeek.ToString();
+
+                var culture = CultureInfo.GetCultureInfo("cs-CZ");
+                var dateTimeInfo = DateTimeFormatInfo.GetInstance(culture);
+
+                var dateTime = DateTime.Today;
+
+                int weekNumber = culture.Calendar.GetWeekOfYear(dateTime, dateTimeInfo.CalendarWeekRule, dateTimeInfo.FirstDayOfWeek);
+
+                //string week = WeeksInYear1. .ToString();
+                string inorout = "Out";
+                s = "INSERT INTO `palogging` (`Time`, `Date`, `Day`, `Week`, `Status`) VALUES('" + time + "', '" + date + "', '" + day + "', '" + weekNumber + "', '" + inorout + "');";
+                connection.Open();
+                cmd = new MySqlCommand(s, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                connection.CloseAsync();
+
+                list();
+                ut();
+            }
+            catch { MessageBox.Show("Error"); }
+        }
+
+        public void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            list();
+        }
+
+        private void button_NesteUke_Click(object sender, EventArgs e)
+        {
+            int c = Int32.Parse(comboBox1.Text);
+            int s = c + 1;
+            comboBox1.Text = s.ToString();
+        }
+
+        private void button_Forje_Click(object sender, EventArgs e)
+        {
+            int c = Int32.Parse(comboBox1.Text);
+            int s = c - 1;
+            comboBox1.Text = s.ToString();
+        }
+        public void list()
+        {
+            string connectionString;
+            connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=##############;password=################";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string command = "SELECT time,date,day,Status FROM palogging WHERE week = '" + comboBox1.Text + "' ;";
+
+            try
+            {
+
+                MySqlDataAdapter MyDA = new MySqlDataAdapter();
+                MyDA.SelectCommand = new MySqlCommand(command, connection);
+                DataTable table = new DataTable();
+                MyDA.Fill(table);
+
+                BindingSource bSource = new BindingSource();
+                bSource.DataSource = table;
+
+
+                dataGridView1.DataSource = bSource;
+            }
+            catch { }
+
+        }
+        public void inn()
+        {
+            string connectionString;
+            connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=#############;password=################";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                string status = "In";
+                connection.Open();
+                MySqlDataAdapter MyDA = new MySqlDataAdapter();
+                string sqlWeek = "SELECT date,time from palogging where status = '" + status + "';";
+                var cmd = new MySqlCommand(sqlWeek, connection);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if ((reader.GetString("date")) == DateTime.Now.ToShortDateString())
+                    {
+                        label_innidag_value.Text = (reader.GetString("time"));
+                    }
+                    else { }
+                }
+            }
+            catch { }
+        }
+        public void ut()
+        {
+            string connectionString;
+            connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=############;password=#######";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                string status = "Out";
+                connection.Open();
+                MySqlDataAdapter MyDA = new MySqlDataAdapter();
+                string sqlWeek = "SELECT date,time from palogging where status = '" + status + "';";
+                var cmd = new MySqlCommand(sqlWeek, connection);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if ((reader.GetString("date")) == DateTime.Now.ToShortDateString())
+                    {
+                        label_utidag_value.Text = (reader.GetString("time"));
+                    }
+                    else { }
+                }
+            }
+            catch { }
+        }
+        public TimeSpan ts;
+
+        public void ptid()
+        {
+            try
+            {
+                string intime = label_innidag_value.Text.ToString().Replace(".", ":");
+                DateTime when = DateTime.Parse(intime);
+                TimeSpan ts = DateTime.Now.Subtract(when);
+                label_PåloggetTid_value.Text = ts.Hours.ToString() + " Timer &" + ts.Minutes.ToString() + " Minuter";
+            }
+            catch { }
+        }
+
+        public void otid()
+        {
+            try
+            {
+                string hei = label_PåloggetTid_value.Text.Remove(1);
+                int heiint = int.Parse(hei);
+                if (heiint > 8)
+                {
+                    string intime = label_innidag_value.Text.ToString().Replace(".", ":");
+                    DateTime when = DateTime.Parse(intime);
+                    TimeSpan ts1 = DateTime.Now.Subtract(when);
+
+                    //if (ts.Hours > TimeSpan.Parse())
+                    //{
+                    string hpr = ts1.Hours.ToString();
+                    Int32 cx = Int32.Parse(hpr);
+                    int c = 8;
+                    int ts2 = c - cx;
+                    string ts2s = ts2.ToString().Replace("-", "");
+
+                    string hprm = ts1.Minutes.ToString();
+                    //Int32 ccx = Int32.Parse(hprm);
+                    //int cc = 8;
+                    //int tsc2 = cc - ccx;
+                    string tot = label_OverTId_value.Text = ts2s + " Timer " + hprm + " Minuter".Replace("-", "");
+                    label_OverTId_value.Text = tot;
+
+                }
+                else { }
+
+                //}
+                //else { label_OverTId_value.Text = "0"; }
+
+                //label_OverTId_value.Text = ts.Hours.ToString() + " Timer &" + ts.Minutes.ToString() + " Minuter";
+
+                if (DateTime.Now.ToShortTimeString() == "00.00  ")
+                {
+                    label_innidag_value.ResetText(); label_OverTId_value.ResetText(); label_utidag_value.ResetText(); label_PåloggetTid_value.ResetText();
+                }
+            }
+            catch { }
+        }
+        public void lables()
+        {
+            if (label_innidag_value.Text == "")
+            {
+                string connectionString;
+                connectionString = "datasource=sindre.ninja;database=csharp;port=3306;username=############;password=#######";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                try
+                {
+                    connection.Open();
+                    MySqlDataAdapter MyDA = new MySqlDataAdapter();
+                    string sqlWeek = "SELECT time from palogging WHERE date='" + DateTime.Now.ToShortDateString() + "' and status='in'";
+                    var cmd = new MySqlCommand(sqlWeek, connection);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        label_innidag_value.Text = (reader.GetString("time"));
+                    }
+
+
+                }
+                catch { }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ptid();
+            otid();
+            lables();
+            
+      
+                }
+
+    }
+
+
+}
